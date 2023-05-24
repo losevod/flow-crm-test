095bba11366d97b46a0f7fbd3f9ff0ec4b51625b
LOsevOD <lazar.aleks21@yandex.ru>
2023-05-24 17:58:24 +0300

    SecurityConfig in work...

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.example.application.security;
 
-public class SecurityConfig {
+import com.example.application.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+
 }
